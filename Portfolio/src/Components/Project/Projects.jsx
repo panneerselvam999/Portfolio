654524b37812338fe96fbe1af7f25b90e16f78f8
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-08-13 13:42:01 +0530

    update project description

--- Portfolio/src/Components/Project/Projects.jsx
+++ Portfolio/src/Components/Project/Projects.jsx
@@ -1,75 +1,10 @@
-// import React from "react";
-// import Title from "../Layout/Title.jsx";
-// import ProjectsCard from "./ProjectsCard.jsx";
-// import { Gilber, Alfa, aturister, Edusity,LogReg } from "../../assets/index.js";
-
-// const Project = () => {
-//     return (
-//         <section
-//             id="projects"
-//             className="w-full py-20 border-b-[1px] border-b-black"
-//         >
-//             <Title title={"My Projects"} des={"visit my portfolio"} />
-
-//             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 xl:gap-14">
-//                 <ProjectsCard
-//                     title="Gilber - Portfolio"
-//                     des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-//                     src={Gilber}
-//                 />
-//                 <ProjectsCard
-//                     title="Alfa Service Center"
-//                     des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-//                     src={Alfa}
-//                 />
-//                 <ProjectsCard
-//                     title="American Tourister"
-//                     des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-//                     src={aturister}
-//                 />
-//                 <ProjectsCard
-//                     title="Educity"
-//                     des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-//                     src={Edusity}
-//                 />
-//                 <ProjectsCard
-//                     title="Login and Registration"
-//                     des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-//                     src={LogReg}
-//                 />
-
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default Project;
 import React from "react";
 import Title from "../Layout/Title.jsx";
 import ProjectsCard from "./ProjectsCard.jsx";
-// import {
-//     Gilber,
-//     Alfa,
-//     aturister,
-//     Edusity,
-//     LogReg,
-// } from "../../assets/index.js";
+
 import projects from "../../Constants/projects.json";
 
 const Project = () => {
-    // const imageMap = {
-    //     Gilber: Gilber,
-    //     Alfa: Alfa,
-    //     aturister: aturister,
-    //     Edusity: Edusity,
-    //     LogReg: LogReg,
-        
-    // };
     return (
         <section
             id="projects"
@@ -78,18 +13,16 @@ const Project = () => {
             <Title title={"My Projects"} des={"visit my portfolio"} />
 
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 xl:gap-14">
-                {
-                    projects.projectsData.map((project) => (
-                        <ProjectsCard
-                            key={project.id}
-                            title={project.title}
-                            des={project.des}
-                            img={project.img}
-                            git={project.git}
-                            live={project.live}
-                        />
-                    ))
-                }
+                {projects.projectsData.map((project) => (
+                    <ProjectsCard
+                        key={project.id}
+                        title={project.title}
+                        des={project.des}
+                        img={project.img}
+                        git={project.git}
+                        live={project.live}
+                    />
+                ))}
             </div>
         </section>
     );
--- Portfolio/src/Components/Project/ProjectsCard.jsx
+++ Portfolio/src/Components/Project/ProjectsCard.jsx
@@ -1,49 +1,97 @@
+// import React from "react";
+// import { BsGithub } from "react-icons/bs";
+// import { FaGlobe } from "react-icons/fa";
+
+// const ProjectCard = ({ title, des, img, git, live }) => {
+//     console.log(des);
+//     console.log(typeof (des));
+
+//     return (
+//         <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
+//             <div className="w-full h-[80%] overflow-hidden rounded-lg">
+//                 <img
+//                     className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
+//                     alt={img}
+//                     src={img}
+//                 />
+//             </div>
+//             <div className="w-full mt-5">
+//                 <div className="flex justify-between items-center">
+//                     <h3 className=" text-designColor ">{title}</h3>
+//                     <div>
+//                         <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center to-gray-400 hover:text-designColor duration-300 cursor-pointer">
+//                             <a href={git} target="_blank" rel="noopener noreferrer">
+//                                 <BsGithub />
+//                             </a>
+//                         </span>
+//                         <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center to-gray-400 hover:text-designColor duration-300 cursor-pointer ml-2">
+//                             <a href={live} target="_blank" rel="noopener noreferrer">
+//                                 <FaGlobe />
+//                             </a>
+//                         </span>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     {
+//                         des.map(desData => (
+//                             <p className="mt-3 text-sm tracking-wide hover:text-gray-300  duration-300" key={desData}>
+//                                 {"#"}{desData} {" "}
+//                             </p>
+//                         ))
+//                     }
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProjectCard;
+
 import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { FaGlobe } from "react-icons/fa";
-// import {
-//   Gilber,
-//   Alfa,
-//   aturister,
-//   Edusity,
-//   LogReg,
-
-// } from "../../assets/index.js";
 
 const ProjectCard = ({ title, des, img, git, live }) => {
-  return (
-    <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
-      <div className="w-full h-[80%] overflow-hidden rounded-lg">
-        <img
-          className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
-          alt={img}
-          src={img}
-        />
-      </div>
-      <div className="w-full mt-5">
-        <div className="flex justify-between items-center">
-          <h3 className=" text-designColor ">{title}</h3>
-          <div>
-            <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center to-gray-400 hover:text-designColor duration-300 cursor-pointer">
-              <a href={git} target="_blank" rel="noopener noreferrer">
-                <BsGithub />
-              </a>
-            </span>
-            <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center to-gray-400 hover:text-designColor duration-300 cursor-pointer ml-2">
-              <a href={live} target="_blank" rel="noopener noreferrer">
-                <FaGlobe />
-              </a>
-            </span>
-          </div>
+    return (
+        <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
+            <div className="w-full h-[80%] overflow-hidden rounded-lg">
+                <img
+                    className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
+                    alt={img}
+                    src={img}
+                />
+            </div>
+            <div className="w-full mt-5">
+                <div className="flex justify-between items-center">
+                    <h3 className=" text-designColor ">{title}</h3>
+                    <div>
+                        <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center to-gray-400 hover:text-designColor duration-300 cursor-pointer">
+                            <a href={git} target="_blank" rel="noopener noreferrer">
+                                <BsGithub />
+                            </a>
+                        </span>
+                        <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center to-gray-400 hover:text-designColor duration-300 cursor-pointer ml-2">
+                            <a href={live} target="_blank" rel="noopener noreferrer">
+                                <FaGlobe />
+                            </a>
+                        </span>
+                    </div>
+                </div>
+                <div className="flex gap-2 flex-wrap mt-3">
+                    {Array.isArray(des) ? (
+                        des.map((description, index) => (
+                            <div key={index} className="flex">
+                                <span className=" text-red-500">#</span>
+                                <p className=" cursor-pointer">{description}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p>{des}</p>
+                    )}
+                </div>
+            </div>
         </div>
-        <div>
-          <p className="mt-3 text-sm tracking-wide hover:text-gray-300  duration-300">
-            {des}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProjectCard;
